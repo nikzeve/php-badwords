d95b4567168d32a8453ebd122186d10206c3b0dc
Nicola Maria Zeverino <nikzeve@gmail.com>
2020-06-15 14:16:59 +0200

    push intermedia

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+    $testo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    $parola_da_censurare = $_GET["parola"];
+
+    $censura ='***';
+
+    $testo = str_replace($parola_da_censurare, $censura, $testo);
+
+?>
 
 
- ?>
+<!DOCTYPE html>
+<html>
+ <head>
+     <link rel="stylesheet" href="style.css">
+     <title>Aggiungi censura</title>
+ </head>
+ <body>
+     <div id="container">
+         <p> <?php  echo $testo; echo $parola_da_censurare; echo $censura; ?> </p>
+     </div>
+ </body>
+</html>
